2cda18ce3e95e85c6ece9b7758e0da5e28d63759
IDMorrison <ariel.sadran@hotmail.com>
2020-03-14 13:50:57 -0300

    ...

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 const superagent = require('superagent');
+const fs = require('fs');
 
 function imprimirMuseos(error, respuesta) {
   if (error) {
     throw new Error('algo se rompió', error);
   }
 
-  const cantidad = respuesta.body.count;
   const museos = respuesta.body.results;
+  const cantidad = museos.length;
+  var stringMuseos = '';
+  /*
+  for(var i= 0; i < cantidad; i++){
+    const museo = museos[i];
+    stringMuseos = stringMuseos + `\n${museo.nombre} (${museo.direccion}). Por cualquier consulta comunicarse al ${museo.telefono}.`;
+  }*/
 
-  console.log(`Se encontraron ${cantidad} museos.`);
-  console.log(`El primer museo se llama ${museos[0].nombre}.`)
+  museos.map(museo => {
+    stringMuseos = stringMuseos + `\n${museo.nombre} (${museo.direccion}). Por cualquier consulta comunicarse al ${museo.telefono}.`;
+  })
+  
+  fs.writeFile('./museos.txt', stringMuseos, (error) => {
+    if (error) {
+      throw new Error('algo se rompió', error);
+    }
+    console.log('El archivo fue escrito');
+  })
 }
 
-console.log('Antes de llamar a superagent')
+
 
 superagent
   .get('https://www.cultura.gob.ar/api/v2.0/museos')
